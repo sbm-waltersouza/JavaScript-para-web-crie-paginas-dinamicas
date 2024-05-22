5a1a36b36a381f8525f4cac7e34532757c684fdc
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-05-22 14:51:51 -0300

    while para correr cada indice da lista para tocar som na hora de clicar no botão

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function tocaSomPom() {
-    document.querySelector(listaDeTeclas).play();
+    document.querySelector('#som_tecla_pom').play();
 
 }
 
@@ -7,4 +7,12 @@ function tocaSomPom() {
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 
-listaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0;
+
+while (contador < 9) {
+
+    listaDeTeclas[contador].onclick = tocaSomPom;
+    contador = contador + 1;
+    console.log(contador);
+}
+ 
