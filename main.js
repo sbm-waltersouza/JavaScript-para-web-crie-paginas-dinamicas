43d1f37f4d72255c173fef1db576eabd55131d6f
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-05-22 15:24:58 -0300

    refazendo nome da function

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let contador = 0;
 
 while (contador < 9) {
 
-    listaDeTeclas[contador].onclick = tocaSomPom;
+    listaDeTeclas[contador].onclick = tocaSom;
     contador = contador + 1;
     console.log(contador);
 }
