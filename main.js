98823f7db9b6925d508b54059660840a1541c09a
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-05-22 10:17:04 -0300

    2 exercicios feitos querySelector e onclick com alert ( fui clicado )

---
+++ main.js
@@ -0,0 +1,18 @@
+// primeiro exercicio 
+
+
+// 1) Selecionando elementos com JavaScript
+// Você já sabe como usar o DevTools para inspecionar elementos HTML.Agora,
+//  está trabalhando em um projeto de site musical e precisa interagir com os botões que representam as teclas de um piano.Logo,
+//   seu desafio é: utilize o Javascript para selecionar um botão específico na sua página.
+
+document.querySelector('.tecla_pom');
+document.querySelector('.tecla_clap');
+
+
+// 2) Emitindo um alerta na tela
+// Imagine que você está trabalhando em um projeto de site que inclui elementos interativos como botões que emitem sons,
+//  semelhante a um piano. Até o momento, você já aprendeu como emitir o som utilizando JavaScript,
+//   agora, a sua missão é emitir um alerta escrito “Fui clicado”,
+//  em um botão assim que ele receber um clique.
+
