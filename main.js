b0813c8db44848ab280b388480fd7cbc07fea1d9
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-05-23 11:55:05 -0300

    tirando onclick dos botão adicionados do html

--- main.js
+++ main.js
@@ -21,6 +21,11 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     tecla.onclick = function () {
         tocaSom(idAudio);
-    };
+    }
+
+    tecla.onkeydown = function () {
+        tecla.classList.add('ativa');
+    }
+
 
 }
